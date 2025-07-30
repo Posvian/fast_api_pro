dc360c4af031d0070a316b60268479526da3a52a
Posvian <posvianski@mail.ru>
2025-07-30 21:25:22 +0300

    add relationship to User model

--- src/account/models/user.py
+++ src/account/models/user.py
@@ -1,4 +1,9 @@
-from sqlalchemy.orm import Mapped
+from __future__ import annotations
+
+from typing import Optional
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.core.orm.base import (
     Base,
@@ -18,3 +23,6 @@ class User(Base):
     last_name: Mapped[str_255]
     is_superuser: Mapped[bool_default_false]
     is_active: Mapped[bool_default_true]
+
+    role_id: Mapped[int | None] = mapped_column(ForeignKey("roles.id"))
+    role: Mapped[Optional["Role"]] = relationship(back_populates="users")
