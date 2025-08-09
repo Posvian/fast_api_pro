6910275c79bb3f28b5f3584de7d4543a44a7ec1e
Posvian <posvianski@mail.ru>
2025-08-09 17:02:06 +0300

    add permission relationship

--- src/account/models/role.py
+++ src/account/models/role.py
@@ -1,15 +1,22 @@
 from __future__ import annotations
+
+from typing import Optional
+
 from sqlalchemy.orm import Mapped, relationship
 
+from src.permissions.models.permissions import PermissionRoleAssociation
 
 from src.core.orm.base import (
     Base,
-    str_255,
+    str_255_unique,
 )
 
 
 class Role(Base):
     __tablename__ = "roles"
 
-    name: Mapped[str_255]
-    users: Mapped[list["User"]] = relationship(back_populates="role")
+    name: Mapped[str_255_unique]
+    users: Mapped[Optional[list["User"]]] = relationship(back_populates="role")
+    permission_associations: Mapped[Optional["PermissionRoleAssociation"]] = (
+        relationship(back_populates="role")
+    )
