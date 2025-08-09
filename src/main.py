bfa2660e8f511b2f2697abbb1c5756f7318d54f5
Posvian <posvianski@mail.ru>
2025-08-09 17:03:49 +0300

    refactor main

--- src/main.py
+++ src/main.py
@@ -3,11 +3,7 @@ from typing import Union
 from fastapi import FastAPI, APIRouter
 from core.router import router
 
-app = FastAPI(
-    docs_url='/api/docs',
-    openapi_url='/api/openapi.json'
-)
+app = FastAPI(docs_url="/api/docs", openapi_url="/api/openapi.json")
+
 
 app.include_router(router)
-
-
