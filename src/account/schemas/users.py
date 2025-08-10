b54c70bd789653d0506beff904177a7a1d1132d2
Posvian <posvianski@mail.ru>
2025-08-10 16:55:50 +0300

    made first_name and second_name validators

--- src/account/schemas/users.py
+++ src/account/schemas/users.py
@@ -1,15 +1,35 @@
 import re
 from datetime import datetime
 
+from fastapi import HTTPException
 from pydantic import BaseModel, EmailStr, Field, field_validator
 
 from .role import RoleResponseSchema
+
+
+NAME_SURNAME_PATTERN = re.compile(r"^[а-яА-Яa-zA-Z\-]+$")
 
 
 class BaseUserSchema(BaseModel):
     email: EmailStr
     first_name: str
     last_name: str
+
+    @field_validator("first_name")
+    def validate_first_name(cls, value) -> str:
+        if not NAME_SURNAME_PATTERN.match(value):
+            raise HTTPException(
+                status_code=422, detail="Имя должно состоять только из букв"
+            )
+        return value
+
+    @field_validator("last_name")
+    def validate_last_name(cls, value) -> str:
+        if not NAME_SURNAME_PATTERN.match(value):
+            raise HTTPException(
+                status_code=422, detail="Фамилия должна состоять только из букв"
+            )
+        return value
 
 
 class UserCreateSchema(BaseUserSchema):
