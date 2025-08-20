0fb2ed410723aaafcd222e5f8d87bfff68cec54e
Posvian <posvianski@mail.ru>
2025-08-20 22:50:21 +0300

    add AuthSchema

--- src/authentication/schemas/auth.py
+++ src/authentication/schemas/auth.py
@@ -1,6 +1,11 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 
 class Token(BaseModel):
     access_token: str
     token_type: str
+
+
+class AuthSchema(BaseModel):
+    email: EmailStr
+    password: str
