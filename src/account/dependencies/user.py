36dfb50f248392532b055f6cbb3ef86557b0310e
Posvian <posvianski@mail.ru>
2025-08-21 22:29:49 +0300

    create dependency for user_filter_data

--- src/account/dependencies/user.py
+++ src/account/dependencies/user.py
@@ -9,3 +9,15 @@ async def get_user_service(
     session: AsyncSession = Depends(get_async_session),
 ):
     return UserService(session=session)
+
+
+async def get_user_filters(
+    first_name: str | None = None,
+    last_name: str | None = None,
+    email__eq: str | None = None,
+) -> dict:
+    return {
+        "first_name": first_name,
+        "last_name": last_name,
+        "email": email__eq,
+    }
