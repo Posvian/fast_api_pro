ffa7ba88a72dfb08a864f8ff725c1351b232ce37
Posvian <posvianski@mail.ru>
2025-08-22 21:33:37 +0300

    update get_me_handler

--- src/authentication/routers/auth.py
+++ src/authentication/routers/auth.py
@@ -1,3 +1,5 @@
+from typing import Annotated
+
 from fastapi import APIRouter
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
@@ -6,29 +8,30 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from src.authentication.dependencies.auth import get_auth_service
 from src.authentication.services.auth import AuthenticationService
 from src.core.constants import credentials_exception
-from src.authentication.schemas.auth import AuthSchema, Token
+from src.authentication.schemas.auth import AuthSchema, Token, User
 from src.account.dependencies.user import get_user_service
 from src.account.servicies import UserService
 from src.account.schemas import UserResponseSchema
 
-# from src.authentication.dependencies.auth import get_current_user
+from src.authentication.dependencies.auth import get_current_active_user
 
 router = APIRouter(prefix="/jwt", tags=["AUTHENTICATION"])
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="v1/authentication/jwt/token")
 
 
-# @router.get("/me")
-# async def get_me_handler(current_user: UserResponseSchema = Depends(get_current_user)):
-#     return current_user
+@router.get("/me", response_model=User)
+async def get_me_handler(
+    current_user: UserResponseSchema = Depends(get_current_active_user),
+):
+    return current_user
 
 
 @router.post("/token")
 async def login(
-    form_data: OAuth2PasswordRequestForm = Depends(),
-    user_service: UserService = Depends(get_user_service),
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
     auth_service: AuthenticationService = Depends(get_auth_service),
-):
+) -> Token:
     user = await auth_service.authenticate_user(
         email=form_data.username, password=form_data.password
     )
