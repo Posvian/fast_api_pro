6b28bcebc7281d9e16c086e91c23e11da24d5cb9
Posvian <posvianski@mail.ru>
2025-08-24 22:17:00 +0300

    register custom handlers

--- src/main.py
+++ src/main.py
@@ -2,8 +2,11 @@ from typing import Union
 
 from fastapi import FastAPI, APIRouter
 from src.core.router import router
+from src.exceptions.exception_handlers import ExceptionHandlerManager
 
 app = FastAPI(docs_url="/api/docs", openapi_url="/api/openapi.json")
 
 
 app.include_router(router)
+
+handler_manager = ExceptionHandlerManager.setup(app=app)
